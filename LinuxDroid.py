4014afdc3f21fb2e05a8289862b5344227563f9f
IsNotSepuh <manahanmanatahan@gmail.com>
2024-12-25 14:10:54 +0700

    Update LinuxDroid.py

--- LinuxDroid.py
+++ LinuxDroid.py
@@ -12,7 +12,7 @@ subprocess.call('figlet -f slant "Kali Linux" | lolcat', shell=True)
 subprocess.call('neofetch', shell=True)
 
 # Custom Prompt (simulated as a message)
-print(u"\033[1;34m\n╭──────(\033[1;31m Root@Kali \033[1;34m)-[\033[1;32m \\w \033[1;34m]\n╰────\033[1;36m$ \033[0m")
+print(u"\033[1;34m\n╭──────(\033[1;31m Root㉿Kali \033[1;34m)-[\033[1;32m \\w \033[1;34m]\n╰────\033[1;36m$ \033[0m")
 
 # Display ASCII art
 subprocess.call('figlet -f small "MatrixMan" | lolcat', shell=True)
